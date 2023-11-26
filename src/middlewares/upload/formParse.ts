39a94248fa23713fbe5c33dee0e7895a5a57d66b
Tomahawkiwi <tlegendarme@gmail.com>
2023-11-26 02:06:35 +0100

    Adapted formParse to deliver boolean and not string (when = "true" or "false" only) + adapted in components needed.

--- src/middlewares/upload/formParse.ts
+++ src/middlewares/upload/formParse.ts
@@ -5,6 +5,17 @@ function asyncFormParse(req: any): Promise<{ fields: any; files: any }> {
   return new Promise((resolve, reject) => {
     const form = new Form();
     form.parse(req, async (err, fields, files) => {
+      const keys = Object.keys(fields);
+      keys.forEach((key: any) => {
+        fields[key].forEach(function callback(value: any, index: number) {
+          if (value === "true") {
+            fields[key][index] = true;
+          }
+          if (value === "false") {
+            fields[key][index] = false;
+          }
+        });
+      });
       if (err) reject(err);
       resolve({ fields, files });
     });
